bc40fde9f9fc89da760ca29a6d7da076ffca6919
atharvaparab9160 <116532157+atharvaparab9160@users.noreply.github.com>
2025-03-19 11:17:30 +0530

    Update langchain_utils.py
    new feature of visualization

--- langchain_utils.py
+++ langchain_utils.py
@@ -1,7 +1,4 @@
 import os
-# from dotenv import load_dotenv
-#
-# load_dotenv()
 import streamlit as st
 db_user = st.secrets["db_user"]
 db_password = st.secrets["db_password"]
@@ -9,9 +6,6 @@ port = st.secrets["port"]
 db_host = st.secrets["db_host"]
 db_name = st.secrets["db_name"]
 
-OPENAI_API_KEY = st.secrets["openAi_API_Key"]
-LANGCHAIN_TRACING_V2 = "true"
-LANGCHAIN_API_KEY = st.secrets["langchain_API_Key"]
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain.chains import create_sql_query_chain
 from langchain_openai import ChatOpenAI
@@ -31,10 +25,12 @@ from prompts import final_prompt, answer_prompt
 import streamlit as st
 @st.cache_resource
 def get_chain():
-    print("Creating chain")
+    
     db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{port}/{db_name}")
+    OPENAI_API_KEY = st.secrets["openAi_API_Key"]
     llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
     generate_query = create_sql_query_chain(llm, db,final_prompt)
+    # st.write("QUERY:",generate_query)
     execute_query = QuerySQLDataBaseTool(db=db)
     rephrase_answer = answer_prompt | llm | StrOutputParser()
     # chain = generate_query | execute_query
@@ -52,17 +48,29 @@ def create_history(messages):
     history = ChatMessageHistory()
     for message in messages:
         if message["role"] == "user":
-            history.add_user_message(message["content"])
+            from matplotlib import figure as figuretype
+            if type(message["content"]) == figuretype.Figure :
+                history.add_user_message("Data visualization")
+            else:
+                history.add_user_message(message["content"])
         else:
-            history.add_ai_message(message["content"])
+            from matplotlib import figure as figuretype
+            if type(message["content"]) == figuretype.Figure :
+                history.add_ai_message("Data visualization")
+            else:
+                history.add_ai_message(message["content"])
     return history
 
 def invoke_chain(question,messages):
-    chain = get_chain()
-    history = create_history(messages)
-    response = chain.invoke({"question": question,"top_k":3,"messages":history.messages})
-    history.add_user_message(question)
-    history.add_ai_message(response)
-    return response
+    try:
+        chain = get_chain()
+        history = create_history(messages)
+        response = chain.invoke({"question": question,"top_k":3,"messages":history.messages})
+        history.add_user_message(question)
+        history.add_ai_message(response)
+    except:
+        return "You have reached your daily limit. Please try again tomorrow."   
+    
+
 
 
