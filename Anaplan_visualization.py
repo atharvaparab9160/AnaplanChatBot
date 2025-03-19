dd9f4e1e1458868706b02eacf44e32d794792469
atharvaparab9160 <116532157+atharvaparab9160@users.noreply.github.com>
2025-03-19 18:06:06 +0530

    Update Anaplan_visualization.py

--- Anaplan_visualization.py
+++ Anaplan_visualization.py
@@ -1,6 +1,6 @@
 def get_openai_response(prompt):
     from openai import OpenAI
-    try:
+    if 1:
         client = OpenAI(
           api_key = st.secrets["openAi_API_Key_visual"],
           base_url = st.secrets["BaseURL"]
@@ -16,8 +16,8 @@ def get_openai_response(prompt):
         )
 
         return completion.choices[0].message.content
-    except:
-        return "Limit Ended"
+    # except:
+    #     return "Limit Ended"
     
     
     
