9d836229bb166b4dcd01b670d5a322b795604500
atharvaparab9160 <116532157+atharvaparab9160@users.noreply.github.com>
2025-03-19 11:22:42 +0530

    Update main.py
    adding sidebar to handle 
    both text and visualization

--- main.py
+++ main.py
@@ -1,43 +1,63 @@
+import pandas as pd
+import pickle
+import requests
 import streamlit as st
 st.set_page_config(
         page_title="Anaplan ChatBot",
         page_icon="chart_with_upwards_trend",
         layout="wide",
 )
-
-from openai import OpenAI
-from langchain_utils import invoke_chain
-st.title("Anaplan Chatbot")
-
-# Set OpenAI API key from Streamlit secrets
-client = OpenAI(api_key=st.secrets["openAi_API_Key"])
+from datetime import date
+from streamlit_option_menu import option_menu
 
 
-# Set a default model
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4o-mini"
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    # print("Creating session state")
-    st.session_state.messages = []
+##############################################
+######## importing chatting as text ##########
+##############################################
+import Anaplan_textForm
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+#############################################
+##### importing chatting as Visualization ###
+#############################################
+import Anaplan_visualization
 
-# Accept user input
-if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
 
-    # Display assistant response in chat message container
-    with st.spinner("Generating response..."):
-        with st.chat_message("assistant"):
-            response = invoke_chain(prompt,st.session_state.messages)
-            st.markdown(response)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+class MultiApp:
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, function):
+        self.apps.append({
+            "title": title,
+            "function": function
+        })
+
+    def run(self):
+
+        with st.sidebar:
+            apps_op  = option_menu(
+                menu_title='Anaplan ChatBot',
+                options=['Chat With Data', '---' ,'Get Visualization'],
+                icons=['house-fill',"house-fill","house-fill"],
+                menu_icon='film',
+                default_index=0,
+                styles={
+                    "container": {"padding": "5!important", "background-color": 'black'},
+                    "icon": {"color": "white", "font-size": "23px" },
+                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "5px",
+                                 "--hover-color": "blue"},
+                    "nav-link-selected": {"background-color": "#02ab21"}, }
+            )
+
+
+        if apps_op == "Chat With Data":
+            Anaplan_textForm.app()
+        elif apps_op == "Get Visualization":
+            Anaplan_visualization.app()
+
+
+RunObj = MultiApp()
+RunObj.run()
